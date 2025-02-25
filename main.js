ae81c06c0f1b5096d604d5c2501b47e6e493f578
codingshushu <ro.@ROui-Macmini.local>
2025-02-26 00:21:13 +0900

    rev api

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const API_KEY = `76e5be7d251b4727a1f3145b3b7ba09c`
 let news = [];
 const getLatestNews =async()=>{
-    const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
+    const url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr`);
     console.log("uuu",url);
     const response = await fetch(url)
     const data = await response.json();
