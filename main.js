cdeb9b3e7bb685e4671ad5d2d2537ca08280e15a
codingshushu <ro.@ROui-Macmini.local>
2025-02-28 00:20:45 +0900

    category keyword noimage icon-place

--- main.js
+++ main.js
@@ -1,42 +1,91 @@
-const API_KEY = `76e5be7d251b4727a1f3145b3b7ba09c`
+const API_KEY = `76e5be7d251b4727a1f3145b3b7ba09c`;
 let newsList = [];
+const openBox = document.getElementById("open-search-box");
+openBox.addEventListener("click", openSearchBox);
+const menus = document.querySelectorAll(".menus button");
+menus.forEach((menu) =>
+  menu.addEventListener("click", (event) => getNewsByCategory(event))
+);
+const searchBtn = document.querySelector(".search-button");
+searchBtn.addEventListener("click", (event) => getNewsByKeyword(event));
+console.log("keyword", searchBtn);
+// searchBtn.forEach(input=>input.addEventListener("click",(event)=>getNewsByKeyword(event)))
 
-
-// categoryNews.addEventListener("click",getNewsByCategory);
-// let allNews = document.querySelectorAll("news");
-
-const getLatestNews =async()=>{
-    const url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr`);
-    console.log("uuu",url);
-    const response = await fetch(url)
-    const data = await response.json();
-    console.log("rrr",response)
-    newsList = data.articles;
-    render();
-    console.log("dddd",newsList);
-}
+const getLatestNews = async () => {
+  const url = new URL(
+    `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr`
+  );
+  console.log("uuu", url);
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log("rrr", response);
+  newsList = data.articles;
+  render();
+  console.log("dddd", newsList);
+};
 getLatestNews();
 
+const getNewsByCategory = async (event) => {
+  const category = event.target.textContent.toLowerCase();
+  console.log("category", category);
+  const url = new URL(
+    `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr&category=${category}`
+  );
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log("DDD", data);
+  newsList = data.articles;
+  render();
+};
 
-const render = () =>{
-  const newsHTML = newsList.map(news=>
-    `<div class ="row news">
-    <div class="col-lg-4">
-        <img class="news-img-size" src="${news.urlToImage || "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqEWgS0uxxEYJ0PsOb2OgwyWvC0Gjp8NUdPw&usqp=CAU"}" alt=""/>
-    </div>
-    <div class="col-lg-8">
-        <h2>${news.title}</h2>
-        <p>
-          ${news.description == null || news.description == "" ? "내용없음" : news.description.length > 200 ? news.description.substring(0, 200) + "..." : news.description}
-        </p>
-        <div>
-            ${news.source.name || "no source"} * (${moment(news.publishedAt).fromNow()})
-        </div>
-    </div>
-</div>`).join("");
-console.log("html",newsHTML);
-  document.getElementById("news-stand").innerHTML = newsHTML;
-}
+const getNewsByKeyword = async (event) => {
+  const keyword = document.querySelector("#search-input").value;
+  console.log("keyword", keyword);
+  const url = new URL(
+    `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?q=${keyword}`
+  );
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log("DDllD", data);
+  newsList = data.articles;
+  render();
+};
+
+const textLimit = (text, limit) => {
+  if (!text) return "내용없음";
+  return text.length > limit ? text.substring(0, limit) + "..." : text;
+};
+
+const render = () => {
+    let newsHTML = newsList
+    .map ( (news) => {
+    const newsTitle= news.title ? news.title : "제목 없음" ;
+    const urlImg = news. urlToImage
+      ? news.urlToImage
+      : "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/300px-No_image_available.svg-png";
+    const newsSource = news. source. name ? news.source.name : "출처없음";
+    const momentTime = news.publishedAt
+      ? moment (news.publishedAt) .fromNow()
+      : "날짜없음";
+
+     return `<div class="row news-list">
+   <div class="col-lg-4">
+    <img class="news-img-size" src="${urlImg}"
+  onerror="this.onerror=null; this.src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/300px-No_image_available.svg.png';"/>
+  </div>
+  <div class="col-lg-8">
+  <h2>${newsTitle}</h2>
+  <p>${textLimit(news.description, 200)} </p> 
+  <div>
+  ${newsSource} * ${momentTime}
+  </div>
+  </div> 
+  </div>`;
+  })
+  .join ("");
+
+  document.getElementById ("news-stand"). innerHTML = newsHTML;
+};
 
 // for(let i = 0;i < 20; i++){
 //     console.log("after",i)
@@ -44,42 +93,19 @@ console.log("html",newsHTML);
 
 /* Set the width of the side navigation to 250px */
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-  }
-  
-  /* Set the width of the side navigation to 0 */
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-  }
-  
-  const openSearchBox = () => {
-    let inputArea = document.getElementById("input-area");
-    if (inputArea.style.display === "inline") {
-      inputArea.style.display = "none";
-    } else {
-      inputArea.style.display = "inline";
-    }
-  };
+  document.getElementById("mySidenav").style.width = "250px";
+}
 
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
 
-  // function render(){
-  //   let resultHTML='';
-  //   for(let i=0;i<news.length;i++){
-  //     if(news[i]>0){
-  //       resultHTML += `<div class ="row news">
-  //           <div class="col-lg-4">
-  //               <img class="news-img-size" src ="https://cdn.imweb.me/upload/S20220801dbacf13d10c9e/1f0c6c4c5761a.png"/>
-  //           </div>
-  //           <div class="col-lg-8">
-  //               <h2>코딩 알려주는 누나</h2>
-  //               <p>
-  //                   스터디 참여
-  //               </p>
-  //               <div>
-  //                   kbs * 2024 11.11
-  //               </div>
-  //           </div>
-  //       </div>`
-  //     }
-  //   }
-  // }
+function openSearchBox() {
+  let inputArea = document.getElementById("input-area");
+  if (inputArea.style.display === "inline") {
+    inputArea.style.display = "none";
+  } else {
+    inputArea.style.display = "inline";
+  }
+}
